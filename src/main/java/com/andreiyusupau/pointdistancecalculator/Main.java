a8681eaa2fe15d7bfef91e8907426fc2e44b9f57
andreiyusupau <nevermind.qqq@gmail.com>
2020-09-18 15:17:31 +0300

    Update DAO structure.

--- src/main/java/com/andreiyusupau/pointdistancecalculator/Main.java
+++ src/main/java/com/andreiyusupau/pointdistancecalculator/Main.java
@@ -5,19 +5,16 @@ import com.andreiyusupau.pointdistancecalculator.dao.ConsoleInputPointDAO;
 import com.andreiyusupau.pointdistancecalculator.dao.DAO;
 import com.andreiyusupau.pointdistancecalculator.model.Point;
 import com.andreiyusupau.pointdistancecalculator.service.PointService;
-import com.andreiyusupau.pointdistancecalculator.util.PointReader;
 import com.andreiyusupau.pointdistancecalculator.view.View;
 import com.andreiyusupau.pointdistancecalculator.view.ViewFactory;
 
 public class Main {
     public static void main(String[] args) {
-        PointReader pointReader = new PointReader();
-        DAO<Point> pointDAO = new ConsoleInputPointDAO(pointReader);
+        DAO<Point> pointDAO = new ConsoleInputPointDAO();
         PointService pointService = new PointService(pointDAO);
         ViewFactory viewFactory=ViewFactory.getInstance();
         View view = viewFactory.getView();
         PointController pointController = new PointController(pointService, view);
         pointController.comparePointDistances();
-        pointReader.close();
     }
 }
--- src/main/java/com/andreiyusupau/pointdistancecalculator/dao/ConsoleInputPointDAO.java
+++ src/main/java/com/andreiyusupau/pointdistancecalculator/dao/ConsoleInputPointDAO.java
@@ -1,13 +1,15 @@
 package com.andreiyusupau.pointdistancecalculator.dao;
 
 import com.andreiyusupau.pointdistancecalculator.model.Point;
-import com.andreiyusupau.pointdistancecalculator.util.PointReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ConsoleInputPointDAO implements DAO<Point> {
-    private final PointReader pointReader;
+    private final PointReader pointReader = new PointReader();
 
-    public ConsoleInputPointDAO(PointReader pointReader) {
-        this.pointReader = pointReader;
+    public ConsoleInputPointDAO() {
     }
 
     @Override
@@ -15,5 +17,37 @@ public class ConsoleInputPointDAO implements DAO<Point> {
         int x = pointReader.getCoordinate("x");
         int y = pointReader.getCoordinate("y");
         return new Point(x, y);
+    }
+
+    private static class PointReader {
+
+        private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        private static int readCounter = 0;
+
+        public int getCoordinate(String axisName) {
+            try {
+                while (true) {
+                    System.out.println("Enter the value of " + axisName + ":");
+                    String input = bufferedReader.readLine();
+                    if (input.matches("-?\\d{1,9}")) {
+                        readCounter++;
+                        return Integer.parseInt(input);
+                    } else {
+                        System.err.println("Enter an integer number with length less than 10 digits!");
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading input.");
+            } finally {
+                if (readCounter == 4) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        System.err.println("Error closing reader.");
+                    }
+                }
+            }
+            return 0;
+        }
     }
 }
