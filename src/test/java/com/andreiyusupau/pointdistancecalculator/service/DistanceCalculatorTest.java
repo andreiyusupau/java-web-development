80616e949ccd2405dea4fc077d45f348e969b475
andreiyusupau <nevermind.qqq@gmail.com>
2020-09-18 12:13:26 +0300

    Update tests.

--- src/test/java/com/andreiyusupau/pointdistancecalculator/service/DistanceCalculatorTest.java
+++ src/test/java/com/andreiyusupau/pointdistancecalculator/service/DistanceCalculatorTest.java
@@ -7,26 +7,65 @@ import org.junit.Test;
 public class DistanceCalculatorTest {
 
     @Test
-    public void calculateDistanceIsEqualZeroBetweenNotZeroPoints(){
+    public void comparePointDistancesIsEqualZeroBetweenNotZeroPoints(){
+        Point point1= new Point(2,3);
+        Point point2=new Point(3,2);
+        DistanceCalculator distanceCalculator=new DistanceCalculator();
+        int compareResult=distanceCalculator.comparePointDistances(point1,point2);
+        Assert.assertEquals(0,compareResult);
+    }
+
+    @Test
+    public void comparePointDistancesIsEqualZeroBetweenSameNotZeroPoints(){
         Point point1= new Point(2,3);
         Point point2=new Point(2,3);
-        double distance=DistanceCalculator.calculateDistance(point1,point2);
-        Assert.assertEquals(0.0,distance,0.00000001);
+        DistanceCalculator distanceCalculator=new DistanceCalculator();
+        int compareResult=distanceCalculator.comparePointDistances(point1,point2);
+        Assert.assertEquals(0,compareResult);
     }
 
     @Test
-    public void calculateDistanceIsEqualFiveBetweenPositivePointAndOriginPoint(){
+    public void comparePointDistancesIsEqualZeroBetweenZeroPoints(){
+        Point point1= new Point(0,0);
+        Point point2=new Point(0,0);
+        DistanceCalculator distanceCalculator=new DistanceCalculator();
+        int compareResult=distanceCalculator.comparePointDistances(point1,point2);
+        Assert.assertEquals(0,compareResult);
+    }
+
+    @Test
+    public void comparePointDistancesIsEqualOneWhenFirstPointIsMoreRemoteFromTheOrigin(){
         Point point1= new Point(4,3);
         Point point2=new Point(0,0);
-        double distance=DistanceCalculator.calculateDistance(point1,point2);
-        Assert.assertEquals(5.0,distance,0.00000001);
+        DistanceCalculator distanceCalculator=new DistanceCalculator();
+        int compareResult=distanceCalculator.comparePointDistances(point1,point2);
+        Assert.assertEquals(1,compareResult);
     }
 
     @Test
-    public void calculateDistanceIsEqualFiveBetweenNegativePointAndOriginPoint(){
+    public void comparePointDistancesIsEqualOneWhenFirstPointIsNegativeAndIsMoreRemoteFromTheOrigin(){
         Point point1= new Point(-4,-3);
         Point point2=new Point(0,0);
-        double distance=DistanceCalculator.calculateDistance(point1,point2);
-        Assert.assertEquals(5.0,distance,0.00000001);
+        DistanceCalculator distanceCalculator=new DistanceCalculator();
+        int compareResult=distanceCalculator.comparePointDistances(point1,point2);
+        Assert.assertEquals(1,compareResult);
+    }
+
+    @Test
+    public void comparePointDistancesIsEqualMinusOneWhenSecondPointIsMoreRemoteFromTheOrigin(){
+        Point point1= new Point(4,3);
+        Point point2=new Point(25,1);
+        DistanceCalculator distanceCalculator=new DistanceCalculator();
+        int compareResult=distanceCalculator.comparePointDistances(point1,point2);
+        Assert.assertEquals(-1,compareResult);
+    }
+
+    @Test
+    public void comparePointDistancesIsEqualOneWhenSecondPointIsNegativeAndIsMoreRemoteFromTheOrigin(){
+        Point point1= new Point(5,-3);
+        Point point2=new Point(-129,0);
+        DistanceCalculator distanceCalculator=new DistanceCalculator();
+        int compareResult=distanceCalculator.comparePointDistances(point1,point2);
+        Assert.assertEquals(-1,compareResult);
     }
 }
